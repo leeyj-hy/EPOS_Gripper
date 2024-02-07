634213f96c4a89fd25fa4daef6f5b25ef90d200d
leeyj-hy <dldydwo1230@hanyang.ac.kr>
2024-02-07 16:05:06 +0900

    first commit

---
+++ build/CMakeFiles/EPOS_GRIPPER.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EPOS_GRIPPER.dir/Epos4_force_test.cpp.o"
+  "CMakeFiles/EPOS_GRIPPER.dir/Epos4_force_test.cpp.o.d"
+  "EPOS_GRIPPER"
+  "EPOS_GRIPPER.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EPOS_GRIPPER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
